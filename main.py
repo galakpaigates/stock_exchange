a19cab57b5e20692b3efe1ebd2fa75f9ed9fa755
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-20 15:07:57 +0000

    modified the port parameter in app.run statement in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from website import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port="5000")
+    app.run(debug=False, host="0.0.0.0", port=int(os.getenv("PORT", default=5000)))
